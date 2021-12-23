c8740e012325e6cb6d3521cb71b2f1c9ad5a07a4
hasananamul <akonmdhasan4454@gmail.com>
2021-12-23 23:35:57 +0800

    class_11

---
+++ data.js
@@ -0,0 +1,183 @@
+//ALL DEVELOPERS DATA
+const devs = [
+  {
+    ID: 1,
+    name: "Hasan",
+    Skill: "MERN stack",
+    Sallery: 9000,
+    photo:
+      "<img src='https://lh3.googleusercontent.com/TVBk7durcOTQgO84Alb7aALkONBGV7mxu-koYcbFCKP7tuX7u5dAQlbjI7v-kCnE7YAd7Gd01LzhpA7rlKRJjOwlLdcWK542VXJoKOZIBaqu3C8SiC2uaTZaE7asyO5544JmbTfBtRN3S5IE2MUbTYFuLPe0yPX12MOhalM12MMPwb2b1DOpFTCUXpdmtj2dBpr_j1rlyFCyHpkQo5rKsk_Sq3XOmbmpJO7cPRGxqvhzukB6Yvh5ky4QRUkwjqjxy5O3_LUjl7F_kTR8o-JgJEeDZpahvPmGUrDeP_mSHTB_GG20KTovd3SqKdQlGYzkCfU5oTgrnNqzpl_g96S82BGRkOnKdykpisk6qgtyVKLA8kt36p0r4jRu18NTutQa9u9N-Qj8uoPeFB2EtZKKetBOgcnOePyNQNNk1mE1KjGpHo9TgrNrFxVmpN75jStm3_zwDITS6255yOkIzaylIw7wE-BzJ7TKEGGeyfLIi-XRESfBFoBJnOx-IGbLwzq-9ulXgxXZRWwSpxN2D5QRlJls2kb06ESpWPGZAM9OmvnWWiuYI8aPfuTB02hsQ6RvopOIyfdHwiXpqzeAMlYss9ENdok29276gITLrP3rtTzqEoLOv7r4QXj0PT27RnKxV20xldwECUR9w8i-C6SyDgsm2M0GFurg6L-P8I1yUzJjggKpaVj7cbcXz_GAf-yeMZU1RSFS2Bncy7TNmU6vYS_8=w568-h757-no?authuser=0' />",
+    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae,",
+  },
+  {
+    ID: 2,
+    name: "Poliya",
+    Skill: "Wordpress",
+    Sallery: 6000,
+    photo:
+      '<img src="https://www.simplilearn.com/ice9/free_resources_article_thumb/How_to_Become_a_PHP_Developer.jpg"/>',
+    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae,",
+  },
+
+  {
+    ID: 3,
+    name: "Rafin",
+    Skill: "Laravel",
+    Sallery: 5000,
+    photo:
+      "<img src='https://lh3.googleusercontent.com/WUtymZyRy0213qokwXr-jCerDboUBFoypFNvLGd5awh2Mys9Pi5qTgtlifkRaE3p9YoHYg3iSKVGcsvvs3tTOPuZgMJZXE1C8chHmVyXocVajTPaYbmeOzqbDuCLOSdsWDsUeB3X73Cs6IV07xVC4ydpf4ng3nE5Bi8EkRb_VjtTrLp38Hi_DmVzcSI46GCieIm5w_aTS2feH2Nlc7PWbWaLOJeZO93TT4AJckYVFWiZ8bvty5IcPPjs5HVHVM2ed_Ishl1ntv3r-P-cHbLfCRwohpYjUfK5vGTyRZzaAS0adAGOMZtkCHcZKzZ5gGRJNpdV-z4RuDRYxmIJlJbDfM4S1ohQyKdtN67lPXjrkYy7eU-v45t3bBlfTK8ThZKENdfyIyaY_ygwGqW6-i3VKnZIahSgUBK2IUWSRUE1gPIKUiA1X5nIXE7TdjeGStJQefWEHUzyh7RiTMXLI9BCwaK6Cc9Rv7ex_GYU53aErejmAfN9031yiscpt4dYirJ0XJ9cTxTLEu4ZaqFxCl9RYzHve_97qHskPxWnIKoUQe8wAxg7JpqxjzIC1CtsHX6DBVoPOZw_jbi5iVQhADjP4iFS1P3sT1n0hG8WPG-d6-icwLxikg8xOnSWUwhN3hcN5xY2TYT1mH9l20QetTtdtVimuYbxyXVa-FJg8HWZWchXuusyAxBOQeVpKc6MdyZyBaiSKe1YwsparN7TxR10CJPY=w1280-h720-no?authuser=0' />",
+    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae,",
+  },
+  {
+    ID: 4,
+    name: "Tanbir",
+    Skill: "Go-lang",
+    Sallery: 7000,
+    photo: "<img src='https://img1.kpopmap.com/2017/09/HanYeSul-min.jpg' />",
+    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae,",
+  },
+  {
+    ID: 5,
+    name: "Jubayer",
+    Skill: "Wordpress",
+    Sallery: 7000,
+    photo:
+      "<img src = 'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-600w-1041841363.jpg'/>",
+    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae,",
+  },
+  {
+    ID: 6,
+    name: "Lamiya Akter",
+    Skill: "Wordpress",
+    Sallery: 7000,
+    photo:
+      "<img src = 'https://lh3.googleusercontent.com/AUGFu_QPaP2hMFwmIbZMULuUG-lCdQmod9BsUxy39zYC-I9zpD7jcfXDmRyUNyimvBwEiNeUyjgxkiGYKuAK6JA2l8lk1Wk3FVJmdB3mvsBoetsXqlx7IGNFN81T_h9jzruFJ3-3_XBARq3CAJ6hHGJD3kJ986d-4VlBm7uVOarCHSvRVrGaGf0BNc8Jyazh0bjRW9PzGI394NGmzw9SjfmrcYkdep01jG5X74FVJ5r_eq7dpm3ta5OR8m-fEnTHga2VZwcBBns79B-V6lbWNReW6xB4NM8ukMDb3gF_HHh2DB0STluBUiKhfYQfvxGGIHzCujeeUfDCWCJ0oxM9UN7mR6qIlkUHqyKAoo2_E5qkaRJuTLiyCYMAodd4GqES2pyjMC3KGWWJdElHgAkQc_FTLKlklBlYxOa88HcnK67BGAGxQM-1nVV5FjeCjAUVLjkk2P05DBozcV_u2Ra7q5VShYhyA_Aao-pLj6GST4TskdxnoRIwgSeGTdNt3X6Gfh9cqx0ylhBA2IGbYUSLCtf2OJUA5ZMyWNAYISxhMsAWqWD3WRk1liMCnYHVjSVLfGZqgN6HQLH1kmS_11F3elvC3Ep_gjBiTtx7ykTGNrnhG2O5hjIDxlVf47r7_KlCfkszM5BYNQ01UsG5thx2QfOjGUQsgbhcH2Y-3eln1srKbC0OxuHeJoRLm0xwtIgW-RSkceh5bRPUZwnVazw8a-mH=w240-h320-no?authuser=0'/>",
+    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae,",
+  },
+  {
+    ID: 7,
+    name: "Emarat",
+    Skill: "Java",
+    Sallery: 9000,
+    photo:
+      "<img src = 'https://st2.depositphotos.com/2783505/8415/i/600/depositphotos_84158918-stock-photo-passport-picture-of-a-hispanic.jpg'/>",
+    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae,",
+  },
+  {
+    ID: 8,
+    name: "Akhy Akter",
+    Skill: "Phython",
+    Sallery: 6000,
+    photo: "<img src = 'https://visafoto.com/img/source355x388_in.jpg'/>",
+    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae,",
+  },
+];
+
+//STUDENTS DATA LIST
+
+const student = [
+  {
+    Roll: 1,
+    Name: "Rafinul Islam",
+    Location: "Madaripur",
+    Age: 14,
+    Gender: "Male",
+    photo:
+      "<img src='https://lh3.googleusercontent.com/TVBk7durcOTQgO84Alb7aALkONBGV7mxu-koYcbFCKP7tuX7u5dAQlbjI7v-kCnE7YAd7Gd01LzhpA7rlKRJjOwlLdcWK542VXJoKOZIBaqu3C8SiC2uaTZaE7asyO5544JmbTfBtRN3S5IE2MUbTYFuLPe0yPX12MOhalM12MMPwb2b1DOpFTCUXpdmtj2dBpr_j1rlyFCyHpkQo5rKsk_Sq3XOmbmpJO7cPRGxqvhzukB6Yvh5ky4QRUkwjqjxy5O3_LUjl7F_kTR8o-JgJEeDZpahvPmGUrDeP_mSHTB_GG20KTovd3SqKdQlGYzkCfU5oTgrnNqzpl_g96S82BGRkOnKdykpisk6qgtyVKLA8kt36p0r4jRu18NTutQa9u9N-Qj8uoPeFB2EtZKKetBOgcnOePyNQNNk1mE1KjGpHo9TgrNrFxVmpN75jStm3_zwDITS6255yOkIzaylIw7wE-BzJ7TKEGGeyfLIi-XRESfBFoBJnOx-IGbLwzq-9ulXgxXZRWwSpxN2D5QRlJls2kb06ESpWPGZAM9OmvnWWiuYI8aPfuTB02hsQ6RvopOIyfdHwiXpqzeAMlYss9ENdok29276gITLrP3rtTzqEoLOv7r4QXj0PT27RnKxV20xldwECUR9w8i-C6SyDgsm2M0GFurg6L-P8I1yUzJjggKpaVj7cbcXz_GAf-yeMZU1RSFS2Bncy7TNmU6vYS_8=w568-h757-no?authuser=0' />",
+  },
+  {
+    Roll: 2,
+    Name: "Jubayer Akon",
+    Location: "Madaripur",
+    Age: 22,
+    Gender: "Male",
+    photo: "<img src = 'https://visafoto.com/img/source355x388_in.jpg'/>",
+  },
+  {
+    Roll: 3,
+    Name: "Poliya Akter",
+    Location: "Dhaka",
+    Age: 19,
+    Gender: "Female",
+    photo:
+      "<img src = 'https://st2.depositphotos.com/2783505/8415/i/600/depositphotos_84158918-stock-photo-passport-picture-of-a-hispanic.jpg'/>",
+  },
+  {
+    Roll: 4,
+    Name: "Zahin Akon",
+    Location: "Italy",
+    Age: 7,
+    Gender: "Male",
+    photo:
+      "<img src = 'https://lh3.googleusercontent.com/AUGFu_QPaP2hMFwmIbZMULuUG-lCdQmod9BsUxy39zYC-I9zpD7jcfXDmRyUNyimvBwEiNeUyjgxkiGYKuAK6JA2l8lk1Wk3FVJmdB3mvsBoetsXqlx7IGNFN81T_h9jzruFJ3-3_XBARq3CAJ6hHGJD3kJ986d-4VlBm7uVOarCHSvRVrGaGf0BNc8Jyazh0bjRW9PzGI394NGmzw9SjfmrcYkdep01jG5X74FVJ5r_eq7dpm3ta5OR8m-fEnTHga2VZwcBBns79B-V6lbWNReW6xB4NM8ukMDb3gF_HHh2DB0STluBUiKhfYQfvxGGIHzCujeeUfDCWCJ0oxM9UN7mR6qIlkUHqyKAoo2_E5qkaRJuTLiyCYMAodd4GqES2pyjMC3KGWWJdElHgAkQc_FTLKlklBlYxOa88HcnK67BGAGxQM-1nVV5FjeCjAUVLjkk2P05DBozcV_u2Ra7q5VShYhyA_Aao-pLj6GST4TskdxnoRIwgSeGTdNt3X6Gfh9cqx0ylhBA2IGbYUSLCtf2OJUA5ZMyWNAYISxhMsAWqWD3WRk1liMCnYHVjSVLfGZqgN6HQLH1kmS_11F3elvC3Ep_gjBiTtx7ykTGNrnhG2O5hjIDxlVf47r7_KlCfkszM5BYNQ01UsG5thx2QfOjGUQsgbhcH2Y-3eln1srKbC0OxuHeJoRLm0xwtIgW-RSkceh5bRPUZwnVazw8a-mH=w240-h320-no?authuser=0'/>",
+  },
+  {
+    Roll: 5,
+    Name: "Tanbir Akon",
+    Location: "Madaripur",
+    Age: 18,
+    Gender: "Male",
+    photo:
+      "<img src = 'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-600w-1041841363.jpg'/>",
+  },
+  {
+    Roll: 6,
+    Name: "Juwel Akon",
+    Location: "Italy",
+    Age: 45,
+    Gender: "Male",
+    photo: "<img src='https://img1.kpopmap.com/2017/09/HanYeSul-min.jpg' />",
+  },
+];
+// contact list data
+
+const contactList = [
+  {
+    Name: "Hasan Anamul",
+    PhoneNumber: +6587323710,
+    Adress: "Sinapore",
+    Relation: "Owner",
+  },
+  {
+    Name: "Poliya Akter",
+    PhoneNumber: "+880170719........",
+    Adress: "Bangladesh",
+    Relation: "Love",
+  },
+  {
+    Name: "Tanbir Akon",
+    PhoneNumber: "+8801741191776",
+    Adress: "Bangladesh",
+    Relation: "Brother",
+  },
+  {
+    Name: "Jubayer Akon",
+    PhoneNumber: "+8801709533126",
+    Adress: "Bangladesh",
+    Relation: "Brother",
+  },
+  {
+    Name: "Shahin Akon",
+    PhoneNumber: "+962373739782",
+    Adress: "Italy",
+    Relation: "Brother",
+  },
+  {
+    Name: "Lotif Munshi",
+    PhoneNumber: "+603662732867",
+    Adress: "Malayshiya",
+    Relation: "Mama",
+  },
+  {
+    Name: "Tahmina",
+    PhoneNumber: "+8801748602277",
+    Adress: "Bangladesh",
+    Relation: "Sister",
+  },
+  {
+    Name: "Abdur Rahman",
+    PhoneNumber: "+8801732148515",
+    Adress: "Bangladesh",
+    Relation: "Brother",
+  },
+];
---
+++ script.js
@@ -0,0 +1,41 @@
+let mainDev = document.querySelector(".main_row");
+let student_list = document.querySelector(".student_list");
+let contact_list = document.querySelector(".contact_list");
+devs.map((data) => {
+  mainDev.innerHTML += `
+        <div class="colm">
+          <div class="card">
+            ${data.photo}
+            <div class="name"><h2> ${data.name}</h2></div>
+            <div class="roll"><h3>ID : ${data.ID}</h3></div>
+            <div class="skill"><h4>Skill : ${data.Skill}</h4></div>
+            <div class="sallary"><h4>Sallary : ${data.Sallery}</h4></div>
+            <div class="text">${data.text}</div>
+          </div>
+        </div>
+    `;
+});
+student.map((data) => {
+  student_list.innerHTML += `
+  <tr>
+    <td scope="row">${data.Roll}</td>
+    <td>${data.Name}</td>
+    <td>${data.Location}</td>
+    <td>${data.Gender}</td>
+    <td>${data.Age}</td>
+    <td>${data.photo}</td>
+  </tr>
+  
+  `;
+});
+contactList.map((data, index) => {
+  contact_list.innerHTML += `
+  <tr>
+    <td scope="row">${index}</td>
+    <td scope="row">${data.Name}</td>
+    <td>${data.PhoneNumber}</td>
+    <td>${data.Relation}</td>
+    <td>${data.Adress}</td>
+  </tr>
+    `;
+});
